5bb0dfea2ecc0419acd52e9bee1512d8fc4b8c28
eduardosi25 <eduardosi25@hotmail.com>
2022-06-20 14:50:56 -0500

    name in test

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ app.post('/sms',(req,res)=>{
    // twiml.message('He recibido tu mensaje');
    // res.writeHead(200,{'Content-Type': 'text/xml'});
    // res.end(twiml.toString());
-   console.log(req.body)
+   console.log("req",req)
+   console.log("res",res)
    const client = require('twilio')(accountSid, authToken);
 
         console.log(from_phone)
